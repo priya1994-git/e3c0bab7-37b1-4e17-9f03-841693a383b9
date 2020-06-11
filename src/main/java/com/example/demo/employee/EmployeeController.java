7518998fc1c14019fcfda18532d3b17a7f1cc337
Git Server Admin <admin@git-server>
2020-06-11 06:12:56 +0000

    Add initial repository

---
+++ src/main/java/com/example/demo/employee/EmployeeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.employee;
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+	
+    //Put your code here
+	public List<Employee> getEmpList(){
+		//Put your code here
+		return null;		
+	}
+	
+}
---
+++ src/main/java/com/example/demo/employee/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.example.demo.employee;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+	
+	
+	public List<Employee> getEmployees() {
+        //put your code here.
+        return null;
+    }
+	
+	
+
+	
+}
